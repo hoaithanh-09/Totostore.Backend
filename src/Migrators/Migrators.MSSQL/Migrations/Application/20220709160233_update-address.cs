7662db4d2e89e7cc009703da1d16cc74b682347c
hoaithanh-09 <1851120014@sv.ut.edu.vn>
2022-07-09 23:03:26 +0700

    chores: Update database

---
+++ src/Migrators/Migrators.MSSQL/Migrations/Application/20220709160233_update-address.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.MSSQL.Migrations.Application
+{
+    public partial class updateaddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CityCode",
+                schema: "Catalog",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DistrictCode",
+                schema: "Catalog",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WardCode",
+                schema: "Catalog",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityCode",
+                schema: "Catalog",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "DistrictCode",
+                schema: "Catalog",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "WardCode",
+                schema: "Catalog",
+                table: "Addresses");
+        }
+    }
+}
